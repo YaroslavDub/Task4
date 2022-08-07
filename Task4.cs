1015e092a305b2ad8464005037870484cfad650f
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-08 00:11:07 +0300

    Task4

---
+++ Task4.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+if (a > 0) {
+    while (i <= a) {
+        Console.WriteLine(i);
+        i += 2;
+    }
+}
+else if (a < 0 && a % 2 == 0){
+    while (a <= i-4) {
+        Console.WriteLine (a);
+        a += 2;
+    }
+}
+else {
+    a += 1;
+    while (a <= i-4) {
+        Console.WriteLine (a);
+        a += 2;
+    }
+}
